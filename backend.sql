3509146afb59aacacb3f96c90a2181dc3f7409f6
chapman <chapmanlin51@gmail.com>
2020-05-07 11:39:11 +0800

    更新資料表

--- backend.sql
+++ backend.sql
@@ -34,16 +34,6 @@ CREATE TABLE `account` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `account`
---
-
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'admin','0beb4894d1609ab48e11eb855abd83b615821f3c95bfb66e3dd78f78480e4af32c859717f4322fd7cc0aeb180673eb112ec5552dc27ef472d0e582e67e5bacda','admin','admin','N'),(2,'test','0beb4894d1609ab48e11eb855abd83b615821f3c95bfb66e3dd78f78480e4af32c859717f4322fd7cc0aeb180673eb112ec5552dc27ef472d0e582e67e5bacda','test','staff','N'),(3,'test2','0beb4894d1609ab48e11eb855abd83b615821f3c95bfb66e3dd78f78480e4af32c859717f4322fd7cc0aeb180673eb112ec5552dc27ef472d0e582e67e5bacda','test2','accounting','Y'),(4,'test3','0beb4894d1609ab48e11eb855abd83b615821f3c95bfb66e3dd78f78480e4af32c859717f4322fd7cc0aeb180673eb112ec5552dc27ef472d0e582e67e5bacda','test3','staff','N');
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `behavior_log`
 --
 
@@ -56,9 +46,8 @@ CREATE TABLE `behavior_log` (
   `operator` varchar(255) DEFAULT NULL,
   `modifytime` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2707 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3973 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 
 --
 -- Table structure for table `family`
@@ -73,18 +62,28 @@ CREATE TABLE `family` (
   `phone` varchar(255) DEFAULT NULL,
   `address` text CHARACTER SET utf8 DEFAULT NULL,
   `delegate` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `members` text CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `family`
+-- Table structure for table `light_history`
 --
 
-LOCK TABLES `family` WRITE;
-/*!40000 ALTER TABLE `family` DISABLE KEYS */;
-/*!40000 ALTER TABLE `family` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `light_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `light_history` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name_list` text DEFAULT NULL,
+  `total_price` int(11) DEFAULT NULL,
+  `gan_year` int(11) DEFAULT NULL,
+  `family_id` int(11) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `light_record`
@@ -95,24 +94,18 @@ DROP TABLE IF EXISTS `light_record`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `light_record` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `member_id` int(11) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `light_id` int(11) DEFAULT NULL,
   `date` timestamp NULL DEFAULT current_timestamp(),
   `price` int(11) DEFAULT NULL,
   `note` text CHARACTER SET utf8 DEFAULT NULL,
   `family_id` int(11) DEFAULT NULL,
+  `gan_year` int(11) DEFAULT NULL,
+  `history_id` int(11) DEFAULT NULL,
+  `is_del` smallint(6) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `light_record`
---
-
-LOCK TABLES `light_record` WRITE;
-/*!40000 ALTER TABLE `light_record` DISABLE KEYS */;
-/*!40000 ALTER TABLE `light_record` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `light_type`
@@ -126,46 +119,8 @@ CREATE TABLE `light_type` (
   `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `price` int(11) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `light_type`
---
-
-LOCK TABLES `light_type` WRITE;
-/*!40000 ALTER TABLE `light_type` DISABLE KEYS */;
-/*!40000 ALTER TABLE `light_type` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `member`
---
-
-DROP TABLE IF EXISTS `member`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `member` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `birthday_1` varchar(150) CHARACTER SET utf8 DEFAULT NULL,
-  `birthday_2` varchar(150) CHARACTER SET utf8 DEFAULT NULL,
-  `sex` int(11) DEFAULT NULL,
-  `zodiac` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `gan_year` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `family_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `member`
---
-
-LOCK TABLES `member` WRITE;
-/*!40000 ALTER TABLE `member` DISABLE KEYS */;
-/*!40000 ALTER TABLE `member` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -176,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-15 15:01:56
+-- Dump completed on 2020-05-07 11:36:34
